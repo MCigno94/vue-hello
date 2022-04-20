a0b0810cec8f2305edbbf03c8f56f27e696c6bde
MCigno94 <mattiacigno27@gmail.com>
2022-04-20 14:39:11 +0200

    final commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+// Dichiaro il container nella quale verra instanziato vue
+const app = new Vue({
+    el: '#app',
+    // Stampo a schermo un messaggio all’interno di un h1, utilizzando i data
+    data: {
+        title: 'Buongiorno!',
+        image: './img/alba.jpg'
+    }
+})
